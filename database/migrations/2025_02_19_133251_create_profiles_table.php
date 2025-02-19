c6947bfbbc5d475871e9126265e786cf158c5a51
phweonixx <kovalskijegor2@gmail.com>
2025-02-19 15:37:40 +0200

    made first migrations (profiles, posts)

---
+++ database/migrations/2025_02_19_133251_create_profiles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('sex')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->text('profile_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
